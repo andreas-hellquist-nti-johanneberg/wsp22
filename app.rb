cf36ac3810e06404b66fa91c3efc15d00a1fc68a
Andreas Hellquist <ahellqui@gmail.com>
2022-03-09 20:37:15 +0100

    Account editing and stuff

--- app.rb
+++ app.rb
@@ -2,7 +2,9 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'bcrypt'
+require 'digest'
 
+# TODO: I REALLY need to encrypt the cookies
 enable :sessions
 
 def get_db()
@@ -12,6 +14,20 @@ def get_db()
 end
 
 helpers do
+    def user_is_logged_in
+        return session.key?(:user_id)
+    end
+
+    def user_is_admin()
+        if not user_is_logged_in
+            return false
+        end
+
+        db = get_db()
+        result = db.execute("SELECT is_admin FROM users WHERE id = ?", session[:user_id]).first
+        return (not result["is_admin"].zero?)
+    end
+
     def username_from_session_id
         db = get_db()
         p session[:user_id]
@@ -57,7 +73,7 @@ post '/users/logout' do
 end
 
 # TODO: Redirecta om redan inloggad
-get '/users/showlogin' do
+get '/users/login' do
     slim(:"users/login")
 end
 
@@ -82,4 +98,133 @@ post '/users/login' do
         # Wrong password
         "Fel lösenord"
     end
+end
+
+# TODO: Replace username in all these routes with an id so links don't break
+# when usernames change
+get '/users/profile/:username' do
+    db = get_db()
+    result = db.execute("SELECT id, username, is_admin, profile_picture
+                         FROM users
+                         WHERE username = ?", params['username']).first
+    unless result
+        return "User does not exist"
+    end
+
+    slim(:'users/profile', locals:{user_data:result})
+end
+
+get '/users/:username/edit' do
+    if (not user_is_logged_in)
+        halt "Logga in först"
+    end
+
+    unless username_from_session_id() == params["username"] || (user_is_admin)
+        halt 403, "Forbidden osv"
+    end
+
+    db = get_db()
+    result = db.execute("SELECT id, username, profile_picture
+                         FROM users
+                         WHERE username = ?", params['username']).first
+
+    unless result
+        return "User does not exist"
+    end
+
+    slim(:'users/edit', locals:{user_data:result})
+end
+
+post '/users/:username/update' do
+    if (not user_is_logged_in)
+        halt "Logga in först"
+    end
+
+    unless username_from_session_id() == params["username"] || (user_is_admin)
+        halt 403, "Forbidden osv"
+    end
+
+    update_username = false
+    update_password = false
+    update_pfp = false
+
+    db = get_db()
+
+    # TODO: Check for duplicates / handle sql exceptions
+    new_username = params["new_username"]
+    unless new_username.empty?
+        update_username = true
+    else
+        # Workaround until url:s use ids
+        username = params["username"]
+    end
+
+    curr_password = params["curr_password"]
+    new_password = params["new_password"]
+    new_password_confirm = params["confirm_new_password"]
+    unless curr_password.empty?() || new_password.empty?() || new_password_confirm.empty?()
+        result = db.execute("SELECT password_hash FROM users WHERE id = ?", session[:user_id]).first
+
+        pw_hash = result["password_hash"]
+        unless BCrypt::Password.new(pw_hash) == curr_password
+            halt "Felaktigt nuvarande lösenord"
+        end
+
+        unless new_password == new_password_confirm
+            halt "Nya lösenord matchade inte"
+        end
+
+        update_password = true
+    end
+
+    pfp_data = params["new_profile_picture"]
+    if pfp_data
+        content_type_header = pfp_data["head"].chomp.lines[-1]
+        p "CONTENT TYPE: #{content_type}"
+        filetype = content_type_header.split(' ')[-1]
+
+        unless filetype == "image/png" || filetype == "image/jpeg"
+            halt "Only png and jpeg images are supported"
+        end
+
+        update_pfp = true
+    end
+
+    # We do all the updating in the end to avoid partly updating things in the case
+    # of errors
+    if update_username
+        db.execute("UPDATE users
+                    SET username = ?
+                    WHERE id = ?", new_username, session[:user_id])
+
+        # Placeholder until I change to ids in url
+        username = new_username
+    end
+
+    if update_password
+        new_pw_hash = BCrypt::Password.create(new_password)
+        db.execute("UPDATE users
+                    SET password_hash = ?
+                    WHERE id = ?", new_pw_hash, session[:user_id])
+    end
+
+    if update_pfp
+        pfp_ext = filetype.split('/')[-1]
+        tmpfile_path = pfp_data["tempfile"]
+
+        pfp_hash = Digest::SHA256.file(tmpfile_path).hexdigest
+ 
+        path = "./public/user_public_data/#{pfp_hash}.#{pfp_ext}"
+        path_for_db = "/user_public_data/#{pfp_hash}.#{pfp_ext}"
+        
+        #Lägg till path i databas
+        db.execute("UPDATE users
+                    SET profile_picture = ?
+                    WHERE id = ?", path_for_db, session[:user_id])
+        
+        #Spara bilden (skriv innehållet i tempfile till destinationen path)
+        File.write(path, File.read(tmpfile_path))
+    end
+ 
+    redirect "/users/profile/#{username}"
 end
---
+++ db/default.sql
@@ -0,0 +1,60 @@
+CREATE TABLE users (
+    -- If the primary key is an integer it becomes an alias for rowid which
+    -- uses unique 64 bit numbers, so autoincrement and not null are not needed
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    is_admin INTEGER NOT NULL,
+    profile_picture TEXT NOT NULL
+);
+
+CREATE TABLE videos (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    video_src TEXT NOT NULL,
+    -- Stored as a unix timestamp
+    upload_date INTEGER NOT NULL
+);
+
+CREATE TABLE user_video_rating_relation (
+    user_id INTEGER NOT NULL,
+    video_id INTEGER NOT NULL,
+    stars INTEGER NOT NULL
+);
+
+CREATE TABLE video_comments (
+    id INTEGER PRIMARY KEY,
+    video_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    upload_date INTEGER NOT NULL
+);
+
+CREATE TABLE blog_comments (
+    id INTEGER PRIMARY KEY,
+    blog_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    upload_date INTEGER NOT NULL
+);
+
+CREATE TABLE blogposts (
+    id INTEGER PRIMARY KEY,
+    content TEXT NOT NULL,
+    upload_date INTEGER NOT NULL
+);
+
+CREATE TABLE genres (
+    id INTEGER PRIMARY KEY,
+    genre TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE blogpost_genre_relation (
+    blogpost_id INTEGER NOT NULL,
+    genre_id INTEGER NOT NULL
+);
+
+CREATE TABLE video_genre_relation (
+    video_id INTEGER NOT NULL,
+    genre_id INTEGER NOT NULL
+);
